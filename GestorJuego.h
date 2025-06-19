e1b4de4b20b8eec86b9913441206c9b055e39112
PalmitoK <julianfigo01@gmail.com>
2025-06-19 17:08:13 -0400

    gestor.h
    actualización de header gestor

--- GestorJuego.h
+++ GestorJuego.h
@@ -5,11 +5,13 @@
 #include <stack>
 #include <string>
 #include "Tablero.h"
+#include "IA.h" 
 
 class GestorJuego {
 public:
     Tablero tablero;
     char jugador_actual;
+    IA agente_ia; 
     std::stack<Tablero> historial;
     EstadoJuego estado_juego;
     ModoJugador modo_actual; 
@@ -20,12 +22,16 @@ public:
     int puntos_esquina_jugador_x;
     int puntos_esquina_jugador_o;
 
-    GestorJuego();
+    int energia_jugador_x;
+    int energia_jugador_o;
+
+    GestorJuego(int profundidad_ia = 5);
 
     void establecer_modo_jugador(ModoJugador modo); 
     ModoJugador obtener_modo_jugador() const;
 
     bool intentar_mover_jugador(int fila, int columna);
+    void procesar_turno_ia();
     void actualizar_estado_juego();
 
     char obtener_jugador_actual() const;
@@ -42,6 +48,14 @@ public:
 
     int obtener_puntos_esquina_jugador_x() const { return puntos_esquina_jugador_x; }
     int obtener_puntos_esquina_jugador_o() const { return puntos_esquina_jugador_o; }
+
+    int obtener_energia(char jugador) const;
+    bool puede_comprar_habilidad(char jugador, int costo) const;
+    void gastar_energia(char jugador, int costo);
+    void ganar_energia_por_volteo(char jugador, int fichas_volteadas);
+    bool realizar_volteo_forzado(int fila, int columna, char jugador);
+
+    void verificar_dominacion_esquinas_publica(char jugador); 
 
 private:
     void verificar_dominacion_esquinas(char jugador);
