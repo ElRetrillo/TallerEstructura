e901e4cdafbea33bfb9dfb1100435d05f4a6d447
PalmitoK <julianfigo01@gmail.com>
2025-06-19 17:01:10 -0400

    Header
    Actulizacion que era con la GUI

--- GUIReversi.h
+++ GUIReversi.h
@@ -3,39 +3,47 @@
 
 #include "ConfiguracionJuego.h" 
 #include <SFML/Graphics.hpp>
-#include "GestorJuego.h"
+#include "GestorJuego.h" 
 
-class GUIReversi {
+class GUIReversi { 
 public:
     GUIReversi(GestorJuego& gm);
-    void ejecutar();
+    void ejecutar(); 
 
 private:
-    // MIEMBROS: Declarar en este orden exacto para coincidir con el constructor y evitar -Wreorder
+    // MIEMBROS: Declarar en este ORDEN EXACTO para coincidir con el constructor y eliminar -Wreorder
     sf::RenderWindow ventana;
     GestorJuego& gestor_juego; 
 
     sf::Font fuente; 
 
-    // Miembros de Textura y Sprite (inicializarlos temprano si se usan en el constructor)
-    sf::Texture textura_fondo_fin_juego; // Movida aquí para inicializarse antes que el sprite
-    sf::Sprite sprite_fondo_fin_juego;   // Movida aquí para inicializarse después de la textura
+    // Textura y Sprite - declarados juntos para inicialización
+    sf::Texture textura_fondo_fin_juego; 
+    sf::Sprite sprite_fondo_fin_juego;   
 
     // Todos los objetos sf::Text
     sf::Text texto_estado;
     sf::Text texto_boton_reiniciar;
     sf::Text titulo_menu;
+    sf::Text texto_boton_jugar_ia; 
     sf::Text texto_boton_jugar_humano; 
     sf::Text texto_boton_volver_menu;
-    sf::Text texto_boton_intercambiar_color;
-    sf::Text texto_boton_ficha_fantasma;
+    sf::Text texto_boton_intercambiar_color; 
+    sf::Text texto_boton_ficha_fantasma;     
+    sf::Text texto_boton_volteo_forzado; 
+    sf::Text texto_energia_jugador_x; 
+    sf::Text texto_energia_jugador_o; 
+    sf::Text texto_stats_jugador_x; 
+    sf::Text texto_stats_jugador_o; 
 
     // Todos los objetos sf::RectangleShape
     sf::RectangleShape boton_reiniciar;
+    sf::RectangleShape boton_jugar_ia; 
     sf::RectangleShape boton_jugar_humano; 
     sf::RectangleShape boton_volver_menu;
     sf::RectangleShape boton_intercambiar_color;
     sf::RectangleShape boton_ficha_fantasma;
+    sf::RectangleShape boton_volteo_forzado; 
     
     // sf::CircleShape y bool para ficha fantasma
     sf::CircleShape forma_ficha_fantasma;
@@ -50,7 +58,7 @@ private:
 
     // Métodos de la clase
     void inicializar_graficos();
-    void procesar_eventos();
+    void procesar_eventos(); 
     void actualizar();
     void renderizar();
 
@@ -67,10 +75,15 @@ private:
     void dibujar_ficha_fantasma();
     void dibujar_boton_intercambiar_color();
     void dibujar_boton_ficha_fantasma();
+    void dibujar_boton_volteo_forzado(); 
+    void dibujar_texto_energia(); 
 
     void manejar_clic_tablero(int fila, int columna);
 
     void ajustar_vista(unsigned int ancho_ventana, unsigned int alto_ventana);
+
+    void reposicionar_elementos_gui();
+    void dibujar_paneles_laterales();
 };
 
 #endif
